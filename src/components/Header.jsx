66ddfbc8f25da040be944e054f90ef7ecec6c574
diego alvarez <diego.stonerough@gmail.com>
2021-09-10 13:13:05 -0500

    se implemento menu hamburguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,59 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faToggleOn, faToggleOff } from "@fortawesome/free-solid-svg-icons";
+import { faToggleOn, faToggleOff, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import useTheme from "../theme/useTheme";
 
 const Header = () => {
   const {themeStatus, changeTheme} = useTheme();
+  const [menuStatus, setMenuStatus] = useState(false)
+
+  const openCloseMenu = () => {
+    setMenuStatus(!menuStatus)
+  };
   
   return (
     <div className={themeStatus ? "header-dark" : "header-light"}>
-      <div className="logo-container">
-        <NavLink to="/home">
-          <img src={themeStatus ? "https://i.imgur.com/8UF9Gkq.png" : "https://i.imgur.com/9gdr4rO.png"} alt="logo" className="logo"/>
-        </NavLink>        
+      <div className="main-logo-container">
+        {/* <div className="menu-icon-1">
+        {menuStatus
+            ? <FontAwesomeIcon icon={faTimes} className="menu-icon" style={{color: themeStatus ? "rgb(2, 218, 200)" : "red"}} onClick={openCloseMenu} />
+            : <FontAwesomeIcon icon={faBars} className="menu-icon" style={{color: themeStatus ? "rgb(2, 218, 200)" : "red"}} onClick={openCloseMenu} />
+          }
+        </div> */}
+        <div className="logo-container">
+          <NavLink to="/home">
+            <img src={themeStatus ? "https://i.imgur.com/8UF9Gkq.png" : "https://i.imgur.com/9gdr4rO.png"} alt="logo" className="logo"/>
+          </NavLink>
+        </div>
+        <div className="menu-container">       
+          {themeStatus
+            ? <FontAwesomeIcon icon={faToggleOn} className="theme-changer-icon-dark2" onClick={() => {changeTheme()}} />
+            : <FontAwesomeIcon icon={faToggleOff} className="theme-changer-icon-light2" onClick={() => {changeTheme()}} />
+          }
+          {menuStatus
+            ? <FontAwesomeIcon icon={faTimes} className="menu-icon" style={{color: themeStatus ? "rgb(2, 218, 200)" : "red"}} onClick={openCloseMenu} />
+            : <FontAwesomeIcon icon={faBars} className="menu-icon" style={{color: themeStatus ? "rgb(2, 218, 200)" : "red"}} onClick={openCloseMenu} />
+          }
+        </div>               
       </div>
       <div className="nav-bar">
-        {/* {theme.themeStatus
-        ? <FontAwesomeIcon icon={faBars} className="theme-changer-icon-dark" />
-        : <FontAwesomeIcon icon={faBars} className="theme-changer-icon-light" />
-        } */}
         {themeStatus
-        ? <FontAwesomeIcon icon={faToggleOn} className="theme-changer-icon-dark" onClick={() => {changeTheme()}} />
-        : <FontAwesomeIcon icon={faToggleOff} className="theme-changer-icon-light" onClick={() => {changeTheme()}} />
+          ? <FontAwesomeIcon icon={faToggleOn} className="theme-changer-icon-dark" onClick={() => {changeTheme()}} />
+          : <FontAwesomeIcon icon={faToggleOff} className="theme-changer-icon-light" onClick={() => {changeTheme()}} />
         }
         <NavLink exact to="/" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Home</h6></NavLink>
         <NavLink to="/about" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>About</h6></NavLink>
         <NavLink to="/services" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Services</h6></NavLink>
         <NavLink to="/gallery" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Gallery</h6></NavLink>
         <NavLink to="/contact" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Contact</h6></NavLink>        
+      </div>
+      <div className="nav-bar2" style={{display: menuStatus ? "block" : "none"}}>
+      <NavLink exact to="/" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Home</h6></NavLink>
+        <NavLink to="/about" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>About</h6></NavLink>
+        <NavLink to="/services" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Services</h6></NavLink>
+        <NavLink to="/gallery" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Gallery</h6></NavLink>
+        <NavLink to="/contact" activeClassName={themeStatus ? "selected1" : "selected2"} className="option"><h6>Contact</h6></NavLink>
       </div>
     </div>
   )
