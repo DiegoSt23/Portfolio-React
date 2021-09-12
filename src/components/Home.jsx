f60cb4db6d81da8d2921e3f880d3a2d94a8b4b74
diego alvarez <diego.stonerough@gmail.com>
2021-09-12 15:52:48 -0500

    se implemento spinner al abrir pagina

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,21 +10,33 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => { 
+  const [loaderState, setLoaderState] = useState(true);
+
+  setTimeout(() => {
+    setLoaderState(false)
+  }, 3000);
+
   return (
     <div className="main-container">
-      <Router>
-        <Header/>       
-        <Switch >
-          <Route exact path="/" component={Hero} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/services" component={Services} />
-          <Route exact path="/gallery" component={ProjectsGallery} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="*" component={Error} />         
-        </Switch>             
-      </Router>
+      {loaderState 
+        ? <div className="container-dark-theme">
+            <div className="preloader"></div>
+          </div>
+        : <Router>
+            <Header/>       
+            <Switch >
+              <Route exact path="/" component={Hero} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/services" component={Services} />
+              <Route exact path="/gallery" component={ProjectsGallery} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="*" component={Error} />         
+            </Switch>             
+        </Router>
+      }     
     </div>
   )
 };
