4e67a3c93b1d523f0bdbeaed5433dc7b06af18fd
diego alvarez <diego.stonerough@gmail.com>
2021-10-20 10:07:32 -0500

    se quito animacion en contact y se cambio height de header

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,37 +1,14 @@
 import CV from "../assets/CV/CV.pdf";
 import { HashLink } from "react-router-hash-link";
-import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
-import { motion, useAnimation } from "framer-motion";
 
 const Contact = () => {
-  const {ref, inView} = useInView();
-  const animation = useAnimation();
   const cvDiego = CV;
-
-  useEffect(() => {
-    if (inView) {
-      animation.start({
-        scale: 1,
-        opacity: 1,
-        transition: {
-          type: "tween",
-          duration: 1
-        }
-      });      
-    }
-  }, [inView, animation]);
 
   return (
     <section className="contact-background" id="contact">
       <div className="contact-main-container">
         <h2>CONTACT</h2>
-        <motion.div 
-          className="contact-container"
-          ref={ref}
-          initial={{scale: 2, opacity: 0}}
-          animate={animation}
-        >
+        <div className="contact-container">
           <h3>Let's talk!</h3>
           <div className="contact-sub-container-1">
             <div 
@@ -55,7 +32,7 @@ const Contact = () => {
               </a>
             </div>
           </div>
-        </motion.div>
+        </div>
       </div>
       <div className="footer">
         <div className="logo-container"> 
