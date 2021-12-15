d6d2b6835f246153b2f7f808ab107b8bd06538a9
diego alvarez <diego.stonerough@gmail.com>
2021-12-15 11:12:17 -0600

    se corrigio error ortografico en skills

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,13 +6,13 @@ import { SiJavascript } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
 import { SiRedux } from "react-icons/si";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGit } from "@fortawesome/free-brands-svg-icons"
+import { faGit } from "@fortawesome/free-brands-svg-icons";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
 
 const Services = () => {
-  const {ref, inView} = useInView();
+  const { ref, inView } = useInView();
   const animation = useAnimation();
 
   useEffect(() => {
@@ -22,9 +22,9 @@ const Services = () => {
         opacity: 1,
         transition: {
           type: "just",
-          duration: 1
-        }
-      });      
+          duration: 1,
+        },
+      });
     }
   }, [inView, animation]);
 
@@ -33,64 +33,58 @@ const Services = () => {
       <div className="services-container">
         <h2>SKILLS</h2>
         <div className="services-sub-container" ref={ref}>
-          <motion.div 
+          <motion.div
             className="services-sub-container-2"
-            initial={{y: 100, opacity: 0}}
+            initial={{ y: 100, opacity: 0 }}
             animate={animation}
           >
-            <BiCodeAlt className="skill-icon"/>
-            <h3>
-              Web Development
-            </h3>
+            <BiCodeAlt className="skill-icon" />
+            <h3>Web Development</h3>
             <p className="text">
               Dynamic webpages and SPAs development with the use of HTML, CSS,
               JavaScript and React with Hooks, Router and Redux. REST APIs
               consumption.
             </p>
           </motion.div>
-          <motion.div 
+          <motion.div
             className="services-sub-container-2"
-            initial={{y: 100, opacity: 0}}
+            initial={{ y: 100, opacity: 0 }}
             animate={animation}
           >
-            <MdOutlinePhonelink className="skill-icon"/>
-            <h3>
-              Web Design
-            </h3>
+            <MdOutlinePhonelink className="skill-icon" />
+            <h3>Web Design</h3>
             <p className="text">
               Web pages with responsive design, in other words, adaptable for
-              different screen sizes to make them always look good and and easy
+              different screen sizes to make them always look beautiful and easy
               to use.
             </p>
           </motion.div>
-          <motion.div 
+          <motion.div
             className="services-sub-container-2"
-            initial={{y: 100, opacity: 0}}
-            animate={animation}            
+            initial={{ y: 100, opacity: 0 }}
+            animate={animation}
           >
-            <BiGitBranch className="skill-icon"/>
-            <h3>
-              Code Branching
-            </h3>
+            <BiGitBranch className="skill-icon" />
+            <h3>Code Branching</h3>
             <p className="text">
               Managament of code branching with the use of Git which allows to
               teamwork without impacting other developers.
             </p>
           </motion.div>
         </div>
-        <motion.div 
+        <motion.div
           className="icons-container-2"
-          initial={{y: 100, opacity: 0}}
+          initial={{ y: 100, opacity: 0 }}
           animate={animation}
         >
-          <AiFillHtml5/>
-          <DiCss3/>
-          <DiSass/>
-          <SiJavascript/>
-          <GrReactjs/>
-          <SiRedux/>
-          <FontAwesomeIcon icon={faGit}/>
-        </motion.div>        
+          <AiFillHtml5 />
+          <DiCss3 />
+          <DiSass />
+          <SiJavascript />
+          <GrReactjs />
+          <SiRedux />
+          <FontAwesomeIcon icon={faGit} />
+        </motion.div>
       </div>
     </section>
   );
